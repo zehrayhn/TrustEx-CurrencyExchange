6f0086b13bea423b3837b1ce3c651216637c0e4a
Aytuğ Özkaya <aaytugozkaya@gmail.com>
2024-08-14 01:15:54 +0300

    service tests written.

--- trustex-be/src/main/java/com/example/trustex/dto/AddMoneyRequestDto.java
+++ trustex-be/src/main/java/com/example/trustex/dto/AddMoneyRequestDto.java
@@ -2,11 +2,13 @@ package com.example.trustex.dto;
 
 import com.example.trustex.entity.CurrencyCode;
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class AddMoneyRequestDto {
 
     @Column(nullable = false)
